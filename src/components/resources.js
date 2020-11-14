34354c0326a8909ac2fdb3db469fce745532dfd0
Isoterik Software <isoteriksoftware@gmail.com>
2020-11-14 12:01:13 +0100

    Update resources.js

--- src/components/resources.js
+++ src/components/resources.js
@@ -1,11 +1,44 @@
-import { makeStyles } from "@material-ui/core";
-import { useEffect } from "react";
+import { BottomNavigation, BottomNavigationAction, makeStyles } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import { scrollToTop } from "./utils";
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import FavoriteRoundedIcon from '@material-ui/icons/FavoriteRounded';
+import CommentRoundedIcon from '@material-ui/icons/CommentRounded';
 
 const Resources = ({ showFooter }) => {
+    const [navigationIndex, setNavigationIndex] = useState(0);
+
+    const onNavigate = (event, newValue) => {
+        setNavigationIndex(newValue);
+    };
+
     const useStyles = makeStyles(theme => ({
         root: {
             marginTop: theme.spacing(1),
+        },
+
+        navigation: {
+            width: '100%',
+            position: 'fixed',
+            bottom: 0,
+            backgroundColor: '#212121',
+        },
+        navigationAction: {
+            '& .MuiBottomNavigationAction-label': {
+                color: 'white',
+            },
+            '& .MuiBottomNavigationAction-wrapper': {
+                color: 'white',
+            },
+            '& .MuiBottomNavigationAction-label.Mui-selected': {
+                color: theme.palette.secondary.light,
+                fontWeight: 'bold',
+            },
+            '& .Mui-selected': {
+                backgroundColor: '#2d2d2d',
+                padding: '.1rem .5rem .1rem .5rem',
+                borderRadius: '.2rem',
+            },
         },
     }));
 
@@ -19,7 +52,16 @@ const Resources = ({ showFooter }) => {
 
     return (
         <div className={classes.root}>
-            Hello
+            <BottomNavigation
+                value={navigationIndex}
+                onChange={onNavigate}
+                //showLabels
+                className={classes.navigation}
+                >
+                <BottomNavigationAction className={classes.navigationAction} label="Home" icon={<HomeRoundedIcon className="icon"/>} />
+                <BottomNavigationAction className={classes.navigationAction} label="Favorites" icon={<FavoriteRoundedIcon />} />
+                <BottomNavigationAction className={classes.navigationAction} label="Comments" icon={<CommentRoundedIcon />} />
+            </BottomNavigation>
         </div>
     );
 };
