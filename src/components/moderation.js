fd4aecbde9a15dd11f86b428234cdf5b2073ba58
Isoterik Software <isoteriksoftware@gmail.com>
2020-11-17 23:18:34 +0100

    Update moderation.js

--- src/components/moderation.js
+++ src/components/moderation.js
@@ -1,14 +1,17 @@
-import { AppBar, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, IconButton, makeStyles, MenuItem, Paper, Slide, Toolbar, Typography } from "@material-ui/core";
-import { CloudUploadRounded, EditRounded, KeyboardArrowLeftRounded, SearchRounded } from "@material-ui/icons";
+import { AppBar, Button, Card, CardActions, CardContent, CardMedia, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, IconButton, makeStyles, MenuItem, Paper, Slide, Toolbar, Typography } from "@material-ui/core";
+import { CloudUploadRounded, DeleteRounded, EditRounded, KeyboardArrowLeftRounded, SearchRounded } from "@material-ui/icons";
 import { Form, Formik } from "formik";
 import { forwardRef, useEffect, useState } from "react";
-import { Redirect, Route, Switch, useHistory } from "react-router-dom";
+import { Link, Redirect, Route, Switch, useHistory } from "react-router-dom";
 import addResourceImage from '../images/add.svg';
 import editProfileImage from '../images/edit.svg';
 import manageResourceImage from '../images/settings.svg';
 import FormikField from "./formik-field";
 import FormikSelect from "./formik-select";
 import { scrollToTop } from "./utils";
+import documentImage from '../images/folder.svg';
+import pdfImage from '../images/pdf.svg';
+import videoImage from '../images/youtube.svg';
 
 const Transition = forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
@@ -232,6 +235,7 @@ const Home = () => {
 
         dialogContent: {
             fontFamily: theme.fontFamily,
+            marginBottom: '2rem',
         },
 
         dialogAppBar: {
@@ -320,7 +324,7 @@ const Home = () => {
                 TransitionComponent={Transition}
                 fullScreen
             >
-                <AppBar className={classes.dialogAppBar} color="secondary">
+                <AppBar className={classes.dialogAppBar} color="primary">
                     <Toolbar>
                         <IconButton edge="start" color="inherit" onClick={handleCloseEditProfileDialog} aria-label="close">
                             <KeyboardArrowLeftRounded/>
@@ -328,7 +332,7 @@ const Home = () => {
                         <Typography variant="h6" className={classes.dialogTitle}>
                             Edit Profile
                         </Typography>
-                        <Button color="primary" variant="outlined" type="submit" form="edit-profile-form" startIcon={<EditRounded/>}>
+                        <Button color="inherit" variant="outlined" type="submit" form="edit-profile-form" startIcon={<EditRounded/>}>
                             Update
                         </Button>
                     </Toolbar>
@@ -403,7 +407,7 @@ const Home = () => {
                 TransitionComponent={Transition}
                 fullScreen
             >
-                <AppBar className={classes.dialogAppBar} color="secondary">
+                <AppBar className={classes.dialogAppBar} color="primary">
                     <Toolbar>
                         <IconButton edge="start" color="inherit" onClick={handleCloseAddResourceDialog} aria-label="close">
                             <KeyboardArrowLeftRounded/>
@@ -411,7 +415,7 @@ const Home = () => {
                         <Typography variant="h6" className={classes.dialogTitle}>
                             Upload Resources
                         </Typography>
-                        <Button color="primary" variant="outlined" type="submit" form="add-resource-form" startIcon={<CloudUploadRounded/>}>
+                        <Button color="inherit" variant="outlined" type="submit" form="add-resource-form" startIcon={<CloudUploadRounded/>}>
                             Upload
                         </Button>
                     </Toolbar>
@@ -480,7 +484,7 @@ const Home = () => {
                                     variant="outlined"
                                     required
                                     fullWidth
-                                    mutliline
+                                    multiline
                                     rows={6}
                                 />
                                 <FormikField
@@ -500,7 +504,84 @@ const Home = () => {
     );
 };
 
+const ResourceCard = ({ resource = {category: 'Video'}, editResource, removeResource }) => {
+    const useStyles = makeStyles(theme => ({
+        imgContainer: {
+            padding: '3rem',
+            background: theme.resourceCard.background,
+        },
+
+        root: {
+            padding: '2rem 3rem 0 3rem',
+            [theme.breakpoints.down('xs')]: {
+                padding: '2rem 1.3rem 0 1.3rem',
+            },
+            [theme.breakpoints.only('sm')]: {
+                padding: '2rem 2rem 0 2rem',
+            },
+
+            '& img': {
+                width: '100%',
+                height: 'auto',
+            }
+        },
+    }));
+
+    const classes = useStyles();
+    let image = null;
+    if (resource.category === 'Video')
+        image = videoImage;
+    else if (resource.category === 'Material')
+        image = pdfImage;
+    else if (resource.category === 'Textbook')
+        image = pdfImage;
+    else if (resource.category === 'Document')
+        image = documentImage;
+
+    return (
+        <Grid item lg={4} xs={12} sm={6} className={classes.root}>
+            <Card>
+                <div className={classes.imgContainer}>
+                    <CardMedia
+                        component="img"
+                        image={image}
+                    />
+                </div>
+                <CardContent>
+                    <Typography variant="h6" className={classes.title}>Excellent Resource</Typography>
+                    <Typography variant="subtitle1" color="textSecondary">2020-01-12</Typography>
+                    <Typography variant="subtitle1" color="textSecondary">COSC101</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button variant="outlined" color="primary.dark" component={Link} to={`../resource/${resource.id}`}>View</Button>
+                    <Button variant="contained" color="secondary" startIcon={<EditRounded/>} onClick={() => editResource(resource)}>Edit</Button>
+                    <Button variant="contained" color="secondary" startIcon={<DeleteRounded/>} onClick={() => removeResource(resource)}>Remove</Button>
+                </CardActions>
+            </Card>         
+        </Grid>
+    );
+};
+
 const Manage = () => {
+    const [showEditResourceDialog, setShowEditResourceDialog] = useState(false);
+    const [showDeleteResourceDialog, setShowDeleteResourceDialog] = useState(false);
+
+    const handleCloseEditResourceDialog = () => {
+        setShowEditResourceDialog(false);
+    };
+
+    const handleShowEditResourceDialog = (resource) => {
+        setShowEditResourceDialog(true);
+    };
+
+    const handleCloseDeleteResourceDialog = () => {
+        setShowDeleteResourceDialog(false);
+    };
+
+    const handleShowDeleteResourceDialog = (resource) => {
+        setShowDeleteResourceDialog(true);
+    };
+
     const useStyles = makeStyles(theme => ({
         root: {
             marginTop: '3rem',
@@ -535,7 +616,23 @@ const Manage = () => {
 
         resourcesContainer: {
             marginTop: '4rem',
-        }
+        },
+
+        dialogContent: {
+            fontFamily: theme.fontFamily,
+            marginBottom: '2rem',
+        },
+
+        dialogAppBar: {
+            position: 'relative',
+        },
+        dialogTitle: {
+            marginLeft: theme.spacing(2),
+            flex: 1,
+            [theme.breakpoints.down('xs')]: {
+                fontSize: '1rem',
+            }
+        },
     }));
 
     const classes = useStyles();
@@ -585,12 +682,105 @@ const Manage = () => {
             </div>
 
             <div className={classes.resourcesContainer}>
-                <Grid container justify="center" spacing={0} alignItems="stretch">
-                    <Grid item lg={4} xs={12} sm={6} className={classes.optionContainer}>
-                        
-                    </Grid>
+                <Grid container justify="start" spacing={0} alignItems="stretch">
+                    <ResourceCard editResource={handleShowEditResourceDialog} removeResource={handleShowDeleteResourceDialog} resource={{ category: 'Document' }}/> <ResourceCard resource={{ category: 'Textbook' }}/> <ResourceCard/>
+                    <ResourceCard resource={{ category: 'Material' }}/>
                 </Grid>
             </div>
+
+            <Dialog
+                open={showEditResourceDialog} 
+                onClose={handleCloseEditResourceDialog}
+                TransitionComponent={Transition}
+                fullScreen
+            >
+                <AppBar className={classes.dialogAppBar} color="primary">
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" onClick={handleCloseEditResourceDialog} aria-label="close">
+                            <KeyboardArrowLeftRounded/>
+                        </IconButton>
+                        <Typography variant="h6" className={classes.dialogTitle}>
+                            Edit Resource
+                        </Typography>
+                        <Button color="inherit" variant="outlined" type="submit" form="edit-resource-form" startIcon={<EditRounded/>}>
+                            Update
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+                <DialogContent>
+                    <DialogContentText className={classes.dialogContent}>
+                        Enter new values for fields that you want to update. Fields marked with asteriks are required.<br/>
+                        <strong>Please note that you are not allowed to change the category or file of an uploaded resource. If that's your requirement, you'll have
+                        to delete the resource and add it again with the desired configurations.</strong>
+                    </DialogContentText>
+                        <Formik
+                            initialValues={{
+                                
+                            }}
+                            
+                            
+                            onSubmit={(values) => { alert('hello'); }}
+                        >
+                            <Form id="edit-resource-form">
+                            <FormikSelect 
+                                    name="course_code" 
+                                    label="Course Code"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                >
+                                    <MenuItem value="test1">COSC101</MenuItem>
+                                    <MenuItem value="test2">COSC102</MenuItem>
+                                </FormikSelect>
+                                <FormikField
+                                    color="secondary"
+                                    name="resource_name"
+                                    label="Resource Title"
+                                    type="text"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                />
+                                <FormikField
+                                    color="secondary"
+                                    name="resource_description"
+                                    label="Resource Description"
+                                    type="text"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    multiline
+                                    rows={6}
+                                />
+                            </Form>
+                        </Formik>
+                </DialogContent>
+            </Dialog>
+
+            <Dialog
+                open={showDeleteResourceDialog} 
+                onClose={handleCloseDeleteResourceDialog}
+                TransitionComponent={Transition}
+                fullWidth
+            >
+                <DialogTitle>
+                    Confirm Delete
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText className={classes.dialogContent}>
+                        Are you sure you want to permanently remove this resource?<br/>
+                        <strong>Please note that you cannot reverse this process.</strong>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDeleteResourceDialog} variant="contained" color="primary">
+                        Cancel
+                    </Button>
+                    <Button type="submit" variant="contained" color="secondary" startIcon={<DeleteRounded/>}>
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 };
