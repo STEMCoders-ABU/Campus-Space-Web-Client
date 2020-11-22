c954af708a89514456bd8ede6dd918295bc009f3
Isoterik Software <isoteriksoftware@gmail.com>
2020-11-22 01:50:06 +0100

    Update init.js

--- src/init.js
+++ src/init.js
@@ -1,10 +1,10 @@
 const { default: Axios } = require("axios");
 
-export const IS_DEVELOPMENT_MODE = true;
+export const IS_DEVELOPMENT_MODE = false;
 
 export const axiosInstance = () => {
     const instance = Axios.create({
-        baseURL: IS_DEVELOPMENT_MODE ? 'http://localhost/csapi/1.1/' : '',
+        baseURL: IS_DEVELOPMENT_MODE ? 'http://localhost/csapi/1.1/' : 'https://api.campus-space.com.ng/public/index.php/',
         withCredentials: true,
         validateStatus: function (status) {
             return status >= 200 && status < 500;
